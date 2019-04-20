5f950b46d2a8316dc05a0415a722e03bc720fe1e
Spencer Davis <spencerdavis2012@gmail.com>
2019-04-20 14:21:00 -0500

    small changes, swapped over to a data folder with random json people

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
-// var todoRouter = require('./routes/todo');
+
 
 var app = express();
 
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
-// app.use('/todo', todoRouter);
+
 
 
 module.exports = app;
